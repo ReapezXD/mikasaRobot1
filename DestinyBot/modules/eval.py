0fbf03cf50c837710393813047db2be77f926651
KAKASHI HATAKE <111272368+ssid143@users.noreply.github.com>
2022-08-22 13:53:37 +0530

    Update eval.py

--- DestinyBot/modules/eval.py
+++ DestinyBot/modules/eval.py
@@ -122,4 +122,4 @@ CLEAR_HANDLER = CommandHandler("clearlocals", clear, run_async=True)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "Eval Module"
+__mod_name__ = "Eᴠᴀʟ ᴍᴏᴅᴜʟᴇ 🥤"
