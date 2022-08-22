20631e43af4a0270af216a78480f62f6749e1095
KAKASHI HATAKE <111272368+ssid143@users.noreply.github.com>
2022-08-22 14:09:33 +0530

    Update locks.py

--- DestinyBot/modules/locks.py
+++ DestinyBot/modules/locks.py
@@ -559,7 +559,7 @@ Locking bots will stop non-admins from adding bots to the chat.
 ❂ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "Lᴏᴄᴋs 🔐"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
