c4ee810fcf0b7b739b63a0186863373829e2190b
KAKASHI HATAKE <111272368+ssid143@users.noreply.github.com>
2022-08-22 14:06:58 +0530

    Update imgeditor.py

--- DestinyBot/modules/imgeditor.py
+++ DestinyBot/modules/imgeditor.py
@@ -450,4 +450,4 @@ async def cb_handler(client: pbot, query: CallbackQuery):
             await removebg_sticker(client, query.message)
 
 
-__mod_name__ = "Img Editor​"
+__mod_name__ = "Iᴍɢ Eᴅɪᴛᴏʀ 👨‍🎤​"
